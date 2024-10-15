bf8621fe482e9a73d8ffe12e0e21401ffaa725a7
Igor Eremin <eremin.igor@icloud.com>
2024-10-14 22:02:48 -0700

    added more assets and updated structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Header from './components/Header';
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
